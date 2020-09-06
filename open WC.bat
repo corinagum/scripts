7ffed5891317e4469dafc93d401215b5f7f2c6c7
Corina <14900841+corinagum@users.noreply.github.com>
2020-09-05 18:24:00 -0700

    Open Web Chat script

---
+++ open WC.bat
@@ -0,0 +1,1 @@
+wt -p "Command Prompt" -d C:\Users\Corina\dev\BotFramework-WebChat --title "WC Run"; split-pane -p "Command Prompt" -d C:\Users\Corina\dev\BotFramework-WebChat ; split-pane -H -p "Command Prompt" -d C:\Users\Corina\dev\BotFramework-WebChat;
