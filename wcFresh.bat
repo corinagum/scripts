ea6648e42ec8504218837a2ba424908e85a87f97
Corina <14900841+corinagum@users.noreply.github.com>
2020-09-16 10:55:24 -0700

    Fix wcFresh typo

--- wcFresh.bat
+++ wcFresh.bat
@@ -1,5 +1,5 @@
 call git clone git@github.com:corinagum/BotFramework-WebChat.git
 cd BotFramework-WebChat
-call git remote add upstream vgit@github.com:microsoft/BotFramework-WebChat.git
+call git remote add upstream git@github.com:microsoft/BotFramework-WebChat.git
 call npm i
 call code .
